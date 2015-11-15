35365ff2248a60920e8d537e85b89c4ac6b1a001
jonathanpeterwu <jpwu03@yahoo.com>
2015-11-15 17:59:06 -0600

    working version of multi sig verification

--- controllers/verification.js
+++ controllers/verification.js
@@ -2,6 +2,7 @@ var _ = require('underscore');
 var async = require('async');
 var crypto = require('crypto');
 var Notification = require('../models/Notification');
+var Wallet = require('../models/Wallet');
 var secrets = require('../config/secrets');
 var auth = require('../config/auth');
 
@@ -27,22 +28,48 @@ exports.getVerifications = function(req, res, next) {
  * Confirm verification.
  */
 exports.postVerification = function(req, res, next) {
-  Notification.findOne({_id: req.params.id }).populate('transactions sender receiver').exec(function(err, notification) {
+  Notification.findOne({_id: req.params.id }).populate('transaction sender receiver').exec(function(err, notification) {
     req.assert('pin', 'Pin cannot be blank').notEmpty();
 
-    console.log(notification)
     var errors = req.validationErrors();
     if (errors) {
       req.flash('errors', errors);
       return res.redirect('/login');
     }
 
+
     if (notification.receiver.pin == req.body.pin) {
+      // Update notification status
       notification.status = 'read';
       notification.transaction.status = 'completed';
-      notification.sender.balance -= notification.transaction.value;
-      notification.receiver.balance += parseInt(receiverWallet.balance) + parseInt(req.body.value);
-      // save transaction, sender recevier
+
+      Wallet.findOne({'_owner': notification.sender._id}, function(err, senderWallet) {
+        if (err) req.flash('errors', err);
+        Wallet.findOne({'_owner': notification.receiver._id}, function(err, receiverWallet) {
+          if (err) req.flash('errors', err);
+
+          // Update Values
+          senderWallet.balance = parseInt(senderWallet.balance) - parseInt(notification.transaction.value);
+          receiverWallet.balance = parseInt(receiverWallet.balance) + parseInt(notification.transaction.value);
+          notification.transaction.status = 'completed';
+
+          console.log(receiverWallet.balance, senderWallet.balance, notification.transaction.status);
+
+          notification.save(function(err) {
+            if (err) return next(err);
+            notification.transaction.save(function(err) {
+              if (err) return next(err);
+              senderWallet.save(function(err){
+                if (err) return next(err);
+                receiverWallet.save(function(err){
+                  if (err) return next(err);
+                  return res.redirect('/notification/' + notification._id);
+                })
+              })
+            })
+          });
+        });
+      });
     } else {
       req.flash('errors', { msg: 'Invalid pin entry' });
       return res.redirect('/login');
